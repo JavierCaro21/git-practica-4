492a58e1417699954c8b264499f34e11db80e895
JavierCaro21 <javicpe@gmail.com>
2021-08-30 14:29:33 +0200

    sql

--- clientes.sql
+++ clientes.sql
@@ -1,8 +1,9 @@
-create table clientes(
+   create table clientes(
       id int primary key auto_increment,
       nombre varchar(50),
       apellidos varchar(50),
       edad int,
       direccion varchar(50)
    );
-    select * from clientes;
+
+
